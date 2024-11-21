e6483e0301ef72ae23ccb92b07a5b2c4e9ae1530
Daria Chernukha <daria-chernukha@mail.ru>
2024-11-21 14:12:40 +0300

    Added unit tests for Database methods

---
+++ tests/test_database.py
@@ -0,0 +1,12 @@
+from praktikum.database import Database
+
+class TestDatabase:
+    def test_available_buns(self):
+        database = Database()
+        available_buns = database.available_buns()
+        assert len(available_buns) == 3
+
+    def test_available_ingredients(self):
+        database = Database()
+        available_ingredients = database.available_ingredients()
+        assert len(available_ingredients) == 6
